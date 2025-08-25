98d7f371a5add06cc14a2eb8a6428529ac351d49
LucaMezz <LucaMezz@users.noreply.github.com>
2025-08-25 12:58:49 +0000

    deploy: 2e19078ccd7066c506698448072abf8e9a5cc945

---
+++ assets/js/acecf23e.79e16988.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/bot-box/blog","blogTitle":"Blog","authorsListPath":"/bot-box/blog/authors"}')}}]);
--- client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/runner/work/bot-box/bot-box/docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/runner/work/bot-box/bot-box/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
-  require("/home/runner/work/bot-box/bot-box/node_modules/.pnpm/@docusaurus+theme-classic@3.8.1_@types+react@19.1.11_acorn@8.15.0_react-dom@19.1.1_reac_3a126b601192d0e61e09b1b6e28d9a01/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/runner/work/bot-box/bot-box/node_modules/.pnpm/@docusaurus+theme-classic@3.8.1_@types+react@19.1.11_acorn@8.15.0_react-dom@19.1.1_reac_3a126b601192d0e61e09b1b6e28d9a01/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/runner/work/bot-box/bot-box/docs/src/css/custom.css"),
-];
